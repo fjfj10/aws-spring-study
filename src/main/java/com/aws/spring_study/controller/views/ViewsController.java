c695d3ac880eab0df3454e58394c458ff1c4290c
fjfj10 <kimmisuk2218@gmail.com>
2023-09-07 13:27:01 +0900

    타임리프 사용법

--- src/main/java/com/aws/spring_study/controller/views/ViewsController.java
+++ src/main/java/com/aws/spring_study/controller/views/ViewsController.java
@@ -1,13 +1,18 @@
 package com.aws.spring_study.controller.views;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ViewsController {
 
+    //@ResponseBody   // 데이터를 바로 응답 Content-type: text 문자열로 반환
     @GetMapping("/student/register")
-    public String studentRegister() {
+    public String studentRegister(Model model) {
+        // addAttribute(key, value)
+        model.addAttribute("title", "학생 등록");
         // templates의 html파일 이름을 적어준다-> html파일을 응답으로 보낸다(Content-type: text/html;charset=UTF-8)
         return "student_register";
     }
